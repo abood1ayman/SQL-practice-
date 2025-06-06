6c14424786b0eba66c17ca7241de191a5926ab9e
abood1ayman <abdullahayman1127956803@gmail.com>
2025-06-06 14:47:53 +0300

    Create 01_select_basics.sql
    added basic SELECT queries

---
+++ 01_select_basics.sql
@@ -0,0 +1,8 @@
+-- Basic SELECT statement
+SELECT * FROM employees;
+
+-- SELECT specific columns
+SELECT name, age FROM employees;
+
+-- SELECT with alias
+SELECT name AS employee_name FROM employees;
